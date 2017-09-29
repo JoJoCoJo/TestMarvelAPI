d935a0c8540af5d7bb505fe28469b5049da09320
JoJoCoJo <jojocojo@msn.com>
2017-09-29 18:00:34 -0500

    Test MarvelAPI

---
+++ js/marvelapi.js
@@ -0,0 +1,21 @@
+var rootURL = 'http://gateway.marvel.com/v1/public/'
+var apiKeyPublic = '26a81b54082b88e0e8e5baed14a0093f'
+var apiKeyPrivate = '1e019543211f21b4421599360379f3e8376b1c91'
+var ts = Date.now()
+
+var hash = $.md5(ts + apiKeyPrivate + apiKeyPublic)
+
+$(document).ready(function(){
+
+	rootURL = rootURL+'characters?apikey='+apiKeyPublic+'&ts='+ts+'&hash='+hash
+
+	$.ajax({
+		
+		url: rootURL
+
+	}).done(function(response){
+		
+		console.log(response)
+
+	})
+})
