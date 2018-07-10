f0780099d28993ccee89bd9d88bc8771daca6b5f
JoJo CJ <jojocojo@msn.com>
2018-07-10 15:30:14 -0500

    added log

--- js/marvelapi.js
+++ js/marvelapi.js
@@ -5,6 +5,8 @@ var ts = Date.now()
 
 var hash = $.md5(ts + apiKeyPrivate + apiKeyPublic)
 
+console.log('urlMarvel >>>', rootURL+'characters?apikey='+apiKeyPublic+'&ts='+ts+'&hash='+hash)
+
 $(document).ready(function(){
 
 	rootURL = rootURL+'characters?apikey='+apiKeyPublic+'&ts='+ts+'&hash='+hash
